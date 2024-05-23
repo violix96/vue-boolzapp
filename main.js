7dcf1a27b403bf613cf3491f7260026cebf9747c
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-24 01:09:35 +0200

    [UPD]  added millestone 2, to be improved

--- main.js
+++ main.js
@@ -2,69 +2,78 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-
+            activeContact: null,
             contacts: [
                 {
                     name: 'Michele',
                     avatar: 'img/avatar_1.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 16:26',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '16:26',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true,
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true,
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '16:15',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            fromMe: false
                         }
                     ],
                 },
                 {
                     name: 'Fabio',
                     avatar: 'img/avatar_2.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 16:30',
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '16:30',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '16:30',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '16:35',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         }
                     ],
                 },
                 {
                     name: 'Samuele',
                     avatar: 'img/avatar_3.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 10:10',
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '10:10',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '10:20',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '16:15',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -73,15 +82,17 @@ createApp({
                 {
                     name: 'Alessandro B.',
                     avatar: 'img/avatar_4.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 15:30',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -90,15 +101,17 @@ createApp({
                 {
                     name: 'Alessandro L.',
                     avatar: 'img/avatar_5.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 15:30',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -107,37 +120,42 @@ createApp({
                 {
                     name: 'Claudia',
                     avatar: 'img/avatar_6.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 15:50',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         }
                     ],
                 },
                 {
                     name: 'Federico',
                     avatar: 'img/avatar_7.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 15:50',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:50',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -146,26 +164,68 @@ createApp({
                 {
                     name: 'Davide',
                     avatar: 'img/avatar_8.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 15:50',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:50',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            fromMe: true
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'OK!!',
                             status: 'received'
                         }
                     ],
-                }
+                },
+                {
+                    name: 'Fabio',
+                    avatar: 'img/avatar_2.jpg',
+                    lastSeen: 'Ultimo accesso oggi alle 16:30',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '16:30',
+                            message: 'Ciao come stai?',
+                            status: 'sent',
+                            fromMe: true
+                        },
+                        {
+                            date: '16:30',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '16:35',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent',
+                            fromMe: true
+                        }
+                    ],
+                },
             ]
+        }
+    },
+    methods: {
+        selectContact(contact) {
+            this.activeContact = contact;
+        },
+        activeContactLastSeen() {
+            if (this.activeContact) {
+                return this.activeContact.lastSeen;
+            }
+        }
+    },
+    mounted() {
+        if (this.contacts.length > 0) {
+            this.selectContact(this.contacts[0]);
         }
     }
 }).mount("#app");
